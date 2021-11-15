8dbeb6f9f2b615100e05f9a9887ad6f79be6771b
tabios24 <tabiostanners24@gmail.com>
2021-11-15 17:59:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-app-richelle
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-app-richelle)
---
+++ src/app/richelle/richelle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-richelle',
+  templateUrl: './richelle.component.html',
+  styleUrls: ['./richelle.component.css']
+})
+export class RichelleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
